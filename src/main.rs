f87fa094d8ff8f61c230fac9b903c1441d6e2d42
Eliah Kagan <degeneracypressure@gmail.com>
2024-08-23 18:39:04 -0400

    Remove commented-out code
    I left this in the previous commit because it wasn't all from the
    commit before that, but it's not needed anymore.

--- src/main.rs
+++ src/main.rs
@@ -12,19 +12,3 @@ fn main() {
         println!("{:3}  relative? {:5}  absolute? {}", path_str, path.is_relative(), path.is_absolute());
     }
 }
-
-// fn main() {
-//     let drive = Path::new("C:");
-//     for sep in ["/", r"\"] {
-//         let joined = drive.join(sep);
-
-
-//         println!("{}  {}", joined.to_str().expect("valid Unicode"), joined.is_absolute());
-//     }
-
-//     let q = c_drive.join("/");
-//     let r = c_drive.join(r"\");
-//     println("{}")
-//     println!("{}", c_drive.join("/").to_str().expect("valid Unicode"));
-//     println!("{}", c_drive.join(r"\").to_str().expect("valid Unicode"));
-// }
