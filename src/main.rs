249c5c013f4b3ad43c37d2bd39f2e9d3acd064e8
Eliah Kagan <degeneracypressure@gmail.com>
2024-08-23 18:38:38 -0400

    Rewrite to show all information clearly

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,30 @@
 use std::path::Path;
 
 fn main() {
-    let c_drive = Path::new("C:");
-    println!("{}", c_drive.join("/").to_str().expect("valid Unicode"));
-    println!("{}", c_drive.join(r"\").to_str().expect("valid Unicode"));
+    let drive = Path::new("C:");
+    let slash = Path::new("/");
+    let backslash = Path::new(r"\");
+    let drive_slash = drive.join(slash);
+    let drive_backslash = drive.join(backslash);
+
+    for path in [drive, slash, backslash, drive_slash.as_path(), drive_backslash.as_path()] {
+        let path_str = path.to_str().expect("valid Unicode");
+        println!("{:3}  relative? {:5}  absolute? {}", path_str, path.is_relative(), path.is_absolute());
+    }
 }
+
+// fn main() {
+//     let drive = Path::new("C:");
+//     for sep in ["/", r"\"] {
+//         let joined = drive.join(sep);
+
+
+//         println!("{}  {}", joined.to_str().expect("valid Unicode"), joined.is_absolute());
+//     }
+
+//     let q = c_drive.join("/");
+//     let r = c_drive.join(r"\");
+//     println("{}")
+//     println!("{}", c_drive.join("/").to_str().expect("valid Unicode"));
+//     println!("{}", c_drive.join(r"\").to_str().expect("valid Unicode"));
+// }
